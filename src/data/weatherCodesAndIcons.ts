cc16fc63612ec95441826206059e2856291229d4
mario <mariorb2017@gmail.com>
2024-01-29 17:37:43 +0100

    Create weather codes, colors and icons info

---
+++ src/data/weatherCodesAndIcons.ts
@@ -0,0 +1,379 @@
+export const weatherCodesAndIcons = [
+  {
+    codes: [1000],
+    day: {
+      bgColor: 'bg-sky-200',
+      textColor: 'text-black',
+      icon: '/src/assets/weather/clear-day.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-900',
+      textColor: 'text-white',
+      icon: '/src/assets/weather/clear-night.svg',
+    },
+  },
+  {
+    codes: [1003],
+    day: {
+      bgColor: 'bg-gray-300',
+      textColor: 'text-black',
+      icon: '/src/assets/weather/partly-cloudy-day.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-900',
+      textColor: 'text-white',
+      icon: '/src/assets/weather/partly-cloudy-night.svg',
+    },
+  },
+  {
+    codes: [1006],
+    day: {
+      bgColor: 'bg-gray-300',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/cloudy.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/cloudy.svg',
+    },
+  },
+  {
+    codes: [1009],
+    day: {
+      bgColor: 'bg-gray-300',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/overcast.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/overcast.svg',
+    },
+  },
+  {
+    codes: [1030],
+    day: {
+      bgColor: 'bg-gray-300',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/mist.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/mist.svg',
+    },
+  },
+  {
+    codes: [1063],
+    day: {
+      bgColor: 'bg-gray-300',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/partly-cloudy-day-rain.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/partly-cloudy-night-rain.svg',
+    },
+  },
+  {
+    codes: [1066],
+    day: {
+      bgColor: 'bg-gray-300',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/partly-cloudy-day-snow.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/partly-cloudy-night-snow.svg',
+    },
+  },
+  {
+    codes: [1069],
+    day: {
+      bgColor: 'bg-gray-300',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/partly-cloudy-day-sleet.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/partly-cloudy-night-sleet.svg',
+    },
+  },
+  {
+    codes: [1072],
+    day: {
+      bgColor: 'bg-gray-300',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/partly-cloudy-day-drizzle.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/partly-cloudy-night-drizzle.svg',
+    },
+  },
+  {
+    codes: [1087],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/thunderstorms-day.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/thunderstorms-night.svg',
+    },
+  },
+  {
+    codes: [1114, 1117],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/snow.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/snow.svg',
+    },
+  },
+  {
+    codes: [1135],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/fog.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/fog.svg',
+    },
+  },
+  {
+    codes: [1147, 1150, 1153],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/drizzle.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/drizzle.svg',
+    },
+  },
+  {
+    codes: [1168, 1171, 1198, 1201, 1204, 1207],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/sleet.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/sleet.svg',
+    },
+  },
+  {
+    codes: [1183],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/rain.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/rain.svg',
+    },
+  },
+  {
+    codes: [1186, 1189],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/moderate-rain.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/moderate-rain.svg',
+    },
+  },
+  {
+    codes: [1192, 1195, 1243, 1246],
+    day: {
+      bgColor: 'bg-gray-600',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/extreme-rain.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/extreme-rain.svg',
+    },
+  },
+  {
+    codes: [1210, 1213, 1216, 1219],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/snow.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/snow.svg',
+    },
+  },
+  {
+    codes: [1222, 1225, 1258],
+    day: {
+      bgColor: 'bg-gray-600',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/extreme-snow.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/extreme-snow.svg',
+    },
+  },
+  {
+    codes: [1237, 1261],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/hail.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/hail.svg',
+    },
+  },
+  {
+    codes: [1240],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/overcast-rain.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/overcast-rain.svg',
+    },
+  },
+  {
+    codes: [1249],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/overcast-sleet.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/overcast-sleet.svg',
+    },
+  },
+  {
+    codes: [1252],
+    day: {
+      bgColor: 'bg-gray-600',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/extreme-sleet.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/extreme-sleet.svg',
+    },
+  },
+  {
+    codes: [1255],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/overcast-snow.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/overcast-snow.svg',
+    },
+  },
+  {
+    codes: [1264],
+    day: {
+      bgColor: 'bg-gray-600',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/extreme-hail.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/extreme-hail.svg',
+    },
+  },
+  {
+    codes: [1273],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/thunderstorms-rain.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/thunderstorms-rain.svg',
+    },
+  },
+  {
+    codes: [1276],
+    day: {
+      bgColor: 'bg-gray-600',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/thunderstorms-extreme-rain.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/thunderstorms-extreme-rain.svg',
+    },
+  },
+  {
+    codes: [1279],
+    day: {
+      bgColor: 'bg-gray-400',
+      textColor: 'text-black',
+      icon: 'src/assets/weather/thunderstorms-snow.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/thunderstorms-snow.svg',
+    },
+  },
+  {
+    codes: [1282],
+    day: {
+      bgColor: 'bg-gray-600',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/thunderstorms-extreme-snow.svg',
+    },
+    night: {
+      bgColor: 'bg-zinc-950',
+      textColor: 'text-white',
+      icon: 'src/assets/weather/thunderstorms-extreme-snow.svg',
+    },
+  },
+];
