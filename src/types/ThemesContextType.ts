b869ea6846a80403bc323815801a5088ee1d21fc
mario <mariorb2017@gmail.com>
2024-01-30 20:55:35 +0100

    Type themes context

---
+++ src/types/ThemesContextType.ts
@@ -0,0 +1,6 @@
+export type ThemesContextType = {
+  darkMode: boolean;
+  setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+  themeBackground: boolean;
+  setThemeBackground: React.Dispatch<React.SetStateAction<boolean>>;
+};
