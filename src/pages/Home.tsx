3c9da7202f505c4aa1de700cd8b8c5998e6e195e
mario <mariorb2017@gmail.com>
2024-01-30 20:54:51 +0100

    Import dark mode and theme background selector

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
-import { Weather } from '../types/Weather';
+import { WeatherType } from '../types/WeatherType';
 import { weatherCodesAndIcons } from '../data/weatherCodesAndIcons';
+import { useContext } from 'react';
+import { ThemesContext } from '../context/Themes';
+import { ThemesContextType } from '../types/ThemesContextType';
 
-export function Home({ weather }: { weather: Weather }) {
+export function Home({ weather }: { weather: WeatherType }) {
+  const { darkMode, themeBackground } = useContext(
+    ThemesContext
+  ) as ThemesContextType;
   const weatherCode = weatherCodesAndIcons.filter((code) => {
     if (code.codes.includes(weather.current.condition.code)) return code;
     return;
@@ -12,9 +18,21 @@ export function Home({ weather }: { weather: Weather }) {
     : (weatherStyle = weatherCode[0].night);
   return (
     <div
-      className={`${weatherStyle.bgColor} ${weatherStyle.textColor} grow rounded-t-xl flex gap-2 justify-center items-center flex-col transition-all`}
+      className={`${
+        themeBackground
+          ? darkMode
+            ? 'bg-neutral-800'
+            : 'bg-gray-200'
+          : weatherStyle.bgColor
+      } ${
+        themeBackground
+          ? darkMode
+            ? 'text-white'
+            : 'text-black'
+          : weatherStyle.textColor
+      } grow rounded-t-xl flex gap-2 justify-center items-center flex-col transition-all`}
     >
-      <div className="flex items-center">
+      <div className="flex items-center gap-2">
         <img src={weatherStyle.icon} alt="" className="w-48" />
         <div className="flex gap-2 flex-col">
           <div className="flex gap-2">
@@ -30,13 +48,13 @@ export function Home({ weather }: { weather: Weather }) {
           />
           <div>
             <p className="text-4xl">
-              {weather.current.wind_kph.toFixed(0)}
+              {weather.current.wind_kph.toFixed(0)}{' '}
               <span className="text-xl">km/h</span>
             </p>
           </div>
         </div>
       </div>
-      <h1 className="text-7xl">{weather.location.name}</h1>
+      <h1 className="text-5xl sm:text-7xl">{weather.location.name}</h1>
       <div className="flex items-center gap-5 text-xl">
         <p className="text-xl">{weather.location.country}</p>|
         <p>{weather.location.localtime.split(' ')[1]}</p>
