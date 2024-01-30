5e88ac3ca041e853b5621fc132ad5143d483979f
mario <mariorb2017@gmail.com>
2024-01-30 20:53:41 +0100

    Provide themes context

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemesProvider } from './context/Themes.tsx';
 import App from './App.tsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemesProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemesProvider>
   </React.StrictMode>
 );
