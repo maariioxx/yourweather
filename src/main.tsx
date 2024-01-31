f2b818620a2716eaede1f98f7186d0ae82d3f6a4
mario <mariorb2017@gmail.com>
2024-01-31 14:59:03 +0100

    Change Themes context name to Settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemesProvider } from './context/Themes.tsx';
+import { SettingsProvider } from './context/Settings.tsx';
 import App from './App.tsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemesProvider>
+    <SettingsProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemesProvider>
+    </SettingsProvider>
   </React.StrictMode>
 );
