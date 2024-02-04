8a6a6e115223750150cc98f048115b8428ece1f1
mario <mariorb2017@gmail.com>
2024-02-04 13:26:05 +0100

    Remove context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { SettingsProvider } from './context/Settings.tsx';
 import App from './App.tsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SettingsProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </SettingsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
