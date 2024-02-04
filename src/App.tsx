8a601e56b71343a2d414bc475c506da1fca7d6a2
mario <mariorb2017@gmail.com>
2024-02-04 13:25:58 +0100

    Import store state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import { useState, useEffect, useContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 import { Home } from './pages/Home.js';
 import Navbar from './components/Navbar.js';
-import { initialData } from './data/initialData.js';
-import { SettingsContext } from './context/Settings.js';
-import { SettingsContextType } from './types/SettingsContextType.js';
+import { useWeatherStore } from './store/weather.js';
+import { useSettingsStore } from './store/settings.js';
 
 export default function App() {
   const [currentCity, setCurrentCity] = useState('London');
-  const [weather, setWeather] = useState<typeof initialData | null>(
-    initialData
-  );
-  const { darkMode } = useContext(SettingsContext) as SettingsContextType;
+  const setWeather = useWeatherStore((state) => state.setWeather);
+  const [darkMode, setDarkMode, setThemeBackground, setImperialUnits] =
+    useSettingsStore((state) => [
+      state.darkMode,
+      state.setDarkMode,
+      state.setThemeBackground,
+      state.setImperialUnits,
+    ]);
 
+  useEffect(() => {
+    if (localStorage.getItem('city') == null) {
+      localStorage.setItem('city', 'London');
+      setCurrentCity('London');
+    } else {
+      const city = localStorage.getItem('city')!;
+      setCurrentCity(city);
+    }
+  }, []);
+
+  /*eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('http://localhost:3000/weather-api', {
@@ -29,14 +43,23 @@ export default function App() {
   }, [currentCity]);
 
   useEffect(() => {
-    if (localStorage.getItem('city') == null) {
-      localStorage.setItem('city', 'London');
-      setCurrentCity('London');
+    if (localStorage.getItem('dark-mode') == null) {
+      localStorage.setItem('dark-mode', 'false');
     } else {
-      const city = localStorage.getItem('city')!;
-      setCurrentCity(city);
+      setDarkMode(localStorage.getItem('dark-mode') == 'true');
+    }
+    if (localStorage.getItem('theme-background') == null) {
+      localStorage.setItem('theme-background', 'false');
+    } else {
+      setThemeBackground(localStorage.getItem('theme-background') == 'true');
+    }
+    if (localStorage.getItem('imperial-units') == null) {
+      localStorage.setItem('imperial-units', 'false');
+    } else {
+      setImperialUnits(localStorage.getItem('imperial-units') == 'true');
     }
   }, []);
+  /*eslint-enable react-hooks/exhaustive-deps */
 
   return (
     <div
@@ -45,11 +68,7 @@ export default function App() {
       } h-full -mb-52 font-poppins flex flex-col bg-gray-50 dark:bg-black`}
     >
       <Navbar setCurrentCity={setCurrentCity} />
-      <main className="grow flex flex-col dark:bg-neutral-900">
-        <Routes>
-          <Route path="/" element={<Home weather={weather!} />} />
-        </Routes>
-      </main>
+      <Home />
     </div>
   );
 }
